cbf6428b2a42d6e2b4dd093152be203081cb51ee
vitoriamorais-hub <vitoria.morais@kikker.com.br>
2026-03-12 14:54:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ let ctx = canvas.getContext("2d")
 ctx.fillStyle = "#FFFFFF"
 ctx.fillRect(0,0,1200,630)
 
+// linha separadora
+ctx.fillStyle = "#E5E5E5"
+ctx.fillRect(590,150,4,330)
+
+// texto inferior
+ctx.font = "28px Arial"
+ctx.fillStyle = "#555"
+ctx.textAlign = "center"
+ctx.fillText("Cliente da Plataforma Kikker",600,560)
+
 let input = document.getElementById("logoCliente")
 
 if(input.files.length > 0){
@@ -20,8 +30,18 @@ let logoCliente = new Image()
 
 logoCliente.onload = function(){
 
-// logo cliente (lado esquerdo)
-ctx.drawImage(logoCliente,150,215,350,200)
+let maxWidth = 350
+let maxHeight = 200
+
+let ratio = Math.min(maxWidth/logoCliente.width, maxHeight/logoCliente.height)
+
+let width = logoCliente.width * ratio
+let height = logoCliente.height * ratio
+
+let x = 300 - width/2
+let y = 315 - height/2
+
+ctx.drawImage(logoCliente,x,y,width,height)
 
 }
 
@@ -34,13 +54,22 @@ reader.readAsDataURL(input.files[0])
 }
 
 // logo kikker
-
 let logoKikker = new Image()
 
 logoKikker.onload = function(){
 
-// logo kikker (lado direito)
-ctx.drawImage(logoKikker,700,215,350,200)
+let maxWidth = 350
+let maxHeight = 200
+
+let ratio = Math.min(maxWidth/logoKikker.width, maxHeight/logoKikker.height)
+
+let width = logoKikker.width * ratio
+let height = logoKikker.height * ratio
+
+let x = 900 - width/2
+let y = 315 - height/2
+
+ctx.drawImage(logoKikker,x,y,width,height)
 
 }
 
@@ -54,7 +83,7 @@ let canvas = document.getElementById("canvas")
 
 let link = document.createElement("a")
 
-link.download = "arte-kikker.png"
+link.download = "cliente-kikker.png"
 
 link.href = canvas.toDataURL()
 
