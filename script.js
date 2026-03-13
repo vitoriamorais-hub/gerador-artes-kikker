568ef2dd8d8791ea0a1d0b2b90e0d7809720ea78
vitoriamorais-hub <vitoria.morais@kikker.com.br>
2026-03-13 16:04:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ let ctx = canvas.getContext("2d")
 ctx.fillStyle = "#FFFFFF"
 ctx.fillRect(0,0,500,500)
 
-// linha separadora
-ctx.fillStyle = "#E5E5E5"
-ctx.fillRect(248,120,4,260)
-
 let input = document.getElementById("logoCliente")
 
 if(input.files.length > 0){
@@ -24,16 +20,16 @@ let logoCliente = new Image()
 
 logoCliente.onload = function(){
 
-let maxWidth = 160
-let maxHeight = 120
+let maxWidth = 300
+let maxHeight = 180
 
 let ratio = Math.min(maxWidth/logoCliente.width, maxHeight/logoCliente.height)
 
 let width = logoCliente.width * ratio
 let height = logoCliente.height * ratio
 
-let x = 125 - width/2
-let y = 250 - height/2
+let x = 250 - width/2
+let y = 120 - height/2
 
 ctx.drawImage(logoCliente,x,y,width,height)
 
@@ -47,12 +43,17 @@ reader.readAsDataURL(input.files[0])
 
 }
 
+// linha separadora
+ctx.fillStyle = "#DDDDDD"
+ctx.fillRect(100,250,300,3)
+
+
 // logo kikker
 let logoKikker = new Image()
 
 logoKikker.onload = function(){
 
-let maxWidth = 160
+let maxWidth = 220
 let maxHeight = 120
 
 let ratio = Math.min(maxWidth/logoKikker.width, maxHeight/logoKikker.height)
@@ -60,8 +61,8 @@ let ratio = Math.min(maxWidth/logoKikker.width, maxHeight/logoKikker.height)
 let width = logoKikker.width * ratio
 let height = logoKikker.height * ratio
 
-let x = 375 - width/2
-let y = 250 - height/2
+let x = 250 - width/2
+let y = 340 - height/2
 
 ctx.drawImage(logoKikker,x,y,width,height)
 
